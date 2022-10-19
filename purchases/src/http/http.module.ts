82da36403948c63a7e035ed37ddacb0c9e566d2f
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-10-18 21:46:23 -0300

    feat: add prisma

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from '../database/database.module';
+
 import { TestController } from './test.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [TestController],
 })
 export class HttpModule {}
--- purchases/src/http/test.controller.ts
+++ purchases/src/http/test.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+
+import { PrismaService } from '../database/prisma/prisma.service';
+
 import { AuthorizationGuard } from './auth/authorization.guard';
 
 @Controller('test')
 export class TestController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('ok')
   @UseGuards(AuthorizationGuard)
   hello() {
-    return 'ok';
+    return this.prisma.customer.findMany();
   }
 }
