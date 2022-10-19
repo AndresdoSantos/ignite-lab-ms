205f9841167880024cdadfa918bd8c72c24e3d8f
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-10-19 13:38:00 -0300

    feat: base [classroom]

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -6,7 +6,9 @@ import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
 
-import { TestResolver } from './test.resolver';
+import { CoursesResolver } from './graphql/resolvers/course.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
@@ -17,6 +19,6 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [CoursesResolver, EnrollmentsResolver, StudentsResolver],
 })
 export class HttpModule {}
