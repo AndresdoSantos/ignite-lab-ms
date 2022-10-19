fede4bbae442536d3e625d6e99209196e23db614
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-10-19 10:27:30 -0300

    feat: create first mutation

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,8 +1,9 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
 import { ProductsService } from '../../../services/products.services';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver('test')
@@ -10,8 +11,13 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') { title }: CreateProductInput) {
+    return this.productsService.createProduct({ title });
   }
 }
