c17af16503ada9e057f55b42e511e197fda8dcb3
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-10-18 22:05:58 -0300

    feat: add graphql [backend]

--- classroom/src/http/test.resolver.ts
+++ classroom/src/http/test.resolver.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query } from '@nestjs/graphql';
 
 import { PrismaService } from '../database/prisma/prisma.service';
 
 import { AuthorizationGuard } from './auth/authorization.guard';
 
-@Controller('test')
-export class TestController {
+@Resolver('test')
+export class TestResolver {
   constructor(private prisma: PrismaService) {}
 
-  @Get('ok')
+  @Query(() => String)
   @UseGuards(AuthorizationGuard)
   hello() {
-    return this.prisma.customer.findMany();
+    return 'Hello GraphQL';
   }
 }
